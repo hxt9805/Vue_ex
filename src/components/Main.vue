548207a19b55ec38edc30a2d7becdb27c381aa7a
xiantao <329433905@qq.com>
2021-03-22 19:18:33 +0800

    vue_ex1

---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="body">
+    <!-- <div class="one">
+      
+    </div>
+    <div class="two" name="two">
+      two的内容显示
+    </div> -->
+    <AppTodos v-if="todos[0].content[0].isShow" />
+    <AppTwo v-if="todos[1].content[0].isShow" />
+  </main>
+</template>
+
+<script type="text/ecmascript-6">
+
+  import AppTodos from "./ex1/AppTodos";
+  import AppTwo from "./ex2/AppTwo";
+
+  export default {
+    components:{
+      AppTodos,
+      AppTwo,
+    },
+    props:['todos'],
+    
+  }
+</script>
+
+<style scoped>
+/* main */
+.body{
+  margin: 200px 0px 0px;
+  background-color: #efefef;
+  height: 80vh;
+}
+
+</style>
